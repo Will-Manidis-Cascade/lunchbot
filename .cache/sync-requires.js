634b726f70614aaf10ce708b9890587895a806fe
Brandon Wang <git@brandon.wang>
2018-08-23 18:28:40 -0400

    Remove cache files

--- .cache/sync-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-
-exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/bw/Documents/bw/lunchbot/.cache/dev-404-page.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/bw/Documents/bw/lunchbot/src/pages/index.js"))
-}
-
